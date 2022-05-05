505b0b3a9f4ffe330c4c592acf6f7d08bdbf85ee
LucasOlar <lucas.olarreaga@etu.unige.ch>
2022-05-05 18:12:29 +0200

    did daily and search table

--- MovieApp.R
+++ MovieApp.R
@@ -16,115 +16,117 @@ options(spinner.color = "#0dc5c1", spinner.size = 1.4, spinner.type = 6)
 # Show all the possible dubs of movies
 df <- data_find("https://raw.githubusercontent.com/rengalv/Movies-Data-Analysis-Grab-a-Popcorn/master/tmdb_5000_movies.csv")
 
-list_spoken_languages <- unlist(strsplit(df$spoken_languages,","))
+list_spoken_languages <- unlist(strsplit(df$spoken_languages, ","))
 list_spoken_languages <- list_spoken_languages[!duplicated(list_spoken_languages)]
-list_spoken_languages <- append( "All", list_spoken_languages)
+list_spoken_languages <- append("All", list_spoken_languages)
 
 # Show all the possible genres of movies
-list_genres <- unlist(strsplit(df$genres,","))
+list_genres <- unlist(strsplit(df$genres, ","))
 list_genres <- list_genres[!duplicated(list_genres)]
-list_genres <- append( "All", list_genres)
+list_genres <- append("All", list_genres)
 
 
 # We used this to have the lists of... to be able to manipulate them (not directly) :
 
-  # Show all the possible original languages
-  # list_languages <- movies_df[!duplicated(movies_df$original_language), ]
-  # list_languages = list_languages %>%
-  #   select(original_language)
-  # unique(list_languages$original_language)
-  
-  # Show all the possible countries of production
-  # list_country <- unlist(strsplit(movies_df$production_countries,","))
-  # list_country <- list_country[!duplicated(list_country)]
-  #  print(list_country)
+# Show all the possible original languages
+# list_languages <- movies_df[!duplicated(movies_df$original_language), ]
+# list_languages = list_languages %>%
+#   select(original_language)
+# unique(list_languages$original_language)
+
+# Show all the possible countries of production
+# list_country <- unlist(strsplit(movies_df$production_countries,","))
+# list_country <- list_country[!duplicated(list_country)]
+#  print(list_country)
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------
 
 ui <- fluidPage(
   shinythemes::themeSelector(),
   theme = bslib::bs_theme(bootswatch = "superhero"),
-  
-    checkboxInput(
-      inputId = "themeToggle",
-      label = icon("sun")),
-  
+  checkboxInput(
+    inputId = "themeToggle",
+    label = icon("sun")
+  ),
+
   # Application title
   titlePanel(tags$img(src = "MyImage.jpg", height = 200, width = 200)),
-  
+
   # Tabset pannel to choose from each tab
   tabsetPanel(
     id = "tabset",
-    tabPanel("PeliDaily", icon = icon("calendar"), ),
+    tabPanel("PeliDaily", icon = icon("calendar"), 
+             mainPanel(withSpinner(tableOutput("daily")))
+             ),
     tabPanel("PeliRecommend",
-             icon = icon("random"),
-             fluid = TRUE,
-             sidebarLayout(
-               position = "right",
-               sidebarPanel(
-                 h3("MOVIES BASED ON YOUR CRITERIA", align = "center", style = "color:cyan"),
-                 br(),
-                 actionBttn("compute", "SHOW ME A MOVIE", icon = icon("film"), color = "success", style = "fill"), align = "center",
-                 br(),
-                 br(),
-                 br(),
-                 selectInput("genre_1", "GENRES 1", list_genres, selected = NULL, multiple = FALSE, width = NULL),
-                 br(),
-                 selectInput("genre_2", "GENRES 2", list_genres, selected = NULL, multiple = FALSE, width = NULL),
-                 br(),
-                 sliderInput("vote_average", "MINIMUM RATING", min = 0, max = 10, value = 0),
-                 br(),
-                 dateInput("date_before", "FILMS BEFORE", value = "2017-02-03", format = "dd-mm-yyyy"),
-                 br(),
-                 dateInput("date_after", "FILMS AFTER", value = "1916-09-04", format = "dd-mm-yyyy"),
-                 br(),
-                 sliderInput("runtime", "MAXIMUM LENGTH (MINUTES)", min = 0, max = 340, value = 340),
-                 br(),
-                 selectInput("original_language", "ORIGINAL LANGUAGE",
-                             c("All", "Afrikaans" = "af","Arabic" = "ar","Catalan" = "cn","Chinese" = "zh","Czech" = "cs","Danish" = "da","Dutch" = "nl","English" = "en","French" = "fr","German" = "de","Greek" = "el","Hebrew" = "he","Hindi" = "hi","Hungarian" = "hu","Icelandic" = "is","Indonesian" = "id","Italian" = "it","Japanese" = "ja","Korean" = "ko","Kyrgyz" = "ky","Norwegian Bokmål" = "nb","Norwegian" = "no","Pashto" = "ps","Persian" = "fa","Polish" = "pl","Portuguese" = "pt","Romanian" = "ro","Russian" = "ru","Slovenian" = "sl","Spanish" = "es","Swedish" = "sv","Tamil" = "ta","Telugu" = "te","Thai" = "th","Turkish" = "tr","Vietnamese" = "vi"
-                             ),
-                             selected = NULL, multiple = FALSE, width = NULL
-                 ),
-                 br(),
-                 selectInput("spoken_languages", "DUBBED LANGUAGE",list_spoken_languages, selected = NULL, multiple = FALSE, width = NULL
-                 )
-               ),
-               mainPanel(withSpinner(tableOutput("recommend")))
-             )
+      icon = icon("random"),
+      fluid = TRUE,
+      sidebarLayout(
+        position = "right",
+        sidebarPanel(
+          h3("MOVIES BASED ON YOUR CRITERIA", align = "center", style = "color:cyan"),
+          br(),
+          actionBttn("compute", "SHOW ME A MOVIE", icon = icon("film"), color = "success", style = "fill"),
+          align = "center",
+          br(),
+          br(),
+          br(),
+          selectInput("genre_1", "GENRES 1", list_genres, selected = NULL, multiple = FALSE, width = NULL),
+          br(),
+          selectInput("genre_2", "GENRES 2", list_genres, selected = NULL, multiple = FALSE, width = NULL),
+          br(),
+          sliderInput("vote_average", "MINIMUM RATING", min = 0, max = 10, value = 0),
+          br(),
+          dateInput("date_before", "FILMS BEFORE", value = "2017-02-03", format = "dd-mm-yyyy"),
+          br(),
+          dateInput("date_after", "FILMS AFTER", value = "1916-09-04", format = "dd-mm-yyyy"),
+          br(),
+          sliderInput("runtime", "MAXIMUM LENGTH (MINUTES)", min = 0, max = 340, value = 340),
+          br(),
+          selectInput("original_language", "ORIGINAL LANGUAGE",
+            c("All", "Afrikaans" = "af", "Arabic" = "ar", "Catalan" = "cn", "Chinese" = "zh", "Czech" = "cs", "Danish" = "da", "Dutch" = "nl", "English" = "en", "French" = "fr", "German" = "de", "Greek" = "el", "Hebrew" = "he", "Hindi" = "hi", "Hungarian" = "hu", "Icelandic" = "is", "Indonesian" = "id", "Italian" = "it", "Japanese" = "ja", "Korean" = "ko", "Kyrgyz" = "ky", "Norwegian Bokmål" = "nb", "Norwegian" = "no", "Pashto" = "ps", "Persian" = "fa", "Polish" = "pl", "Portuguese" = "pt", "Romanian" = "ro", "Russian" = "ru", "Slovenian" = "sl", "Spanish" = "es", "Swedish" = "sv", "Tamil" = "ta", "Telugu" = "te", "Thai" = "th", "Turkish" = "tr", "Vietnamese" = "vi"),
+            selected = NULL, multiple = FALSE, width = NULL
+          ),
+          br(),
+          selectInput("spoken_languages", "DUBBED LANGUAGE", list_spoken_languages, selected = NULL, multiple = FALSE, width = NULL)
+        ),
+        mainPanel(withSpinner(tableOutput("recommend")))
+      )
     ),
     tabPanel("PeliSearch",
-             icon = icon("search"),
-             fluid = TRUE,
-             sidebarLayout(
-               position = "right",
-               sidebarPanel(
-                 h3("MOVIE SEARCH", align = "center", style = "color:cyan"),
-                 br(),
-                 actionBttn("compute_2", "SHOW ME THE MOVIES", icon = icon("film"), color = "success", style = "fill",), align = "center",
-                 br(),
-                 br(),
-                 br(),
-                 h4("OFFICIAL TITLE"),
-                 textInput(inputId = "title", label = "", value = ""),
-                 br(),
-                 textInput(inputId = "key_words_1", label = "KEY WORDS 1", value = ""),
-                 br(),
-                 textInput(inputId = "key_words_2", label = "KEY WORD 2", value = ""),
-                 br(),
-                 textInput(inputId = "key_words_3", label = "KEY WORD 3", value = ""),
-                 br(),
-                 sliderInput("popularity", "MINIMUM POPULARITY", min = 0, max = 150, value = 0),
-                 br(),
-                 selectInput("country", "PRODUCTION COUNTRY", list(
-                   "All",
-                   "Africa" = c("Algeria", "Angola", "Cameroon", "Egypt", "Kenya", "Libyan Arab Jamahiriya", "Morocco", "Nigeria", "South Africa", "Tunisia"), 
-                   "America" = c("Argentina", "Aruba", "Bahamas", "Bolivia", "Brazil", "Canada", "Chile", "Colombia", "Dominica", "Dominican Republic", "Ecuador", "Guadaloupe", "Guyana", "Jamaica", "Mexico", "Panama", "Peru", "United States of America"),
-                   "Asia-Oceania" = c("Afghanistan", "Australia", "Bhutan", "Cambodia", "China", "Fiji", "Hong Kong", "India", "Indonesia", "Iran", "Israel", "Japan", "Jordan", "Kazakhstan", "Kyrgyz Republic", "Lebanon", "Malaysia", "New Zealand", "Pakistan", "Philippines", "Singapore", "South Korea", "Taiwan", "Thailand", "Turkey", "United Arab Emirates"),
-                   "Europe" = c("Austria", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Cyprus", "Czech Republic", "Denmark", "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Lithuania", "Luxembourg", "Malta", "Monaco", "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Russia", "Serbia", "Serbia and Montenegro", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Ukraine", "United Kingdom")
-                 ), selected = NULL, multiple = FALSE, width = NULL)
-               ),
-               mainPanel(withSpinner(tableOutput("search")))
-             )
+      icon = icon("search"),
+      fluid = TRUE,
+      sidebarLayout(
+        position = "right",
+        sidebarPanel(
+          h3("MOVIE SEARCH", align = "center", style = "color:cyan"),
+          br(),
+          actionBttn("compute_2", "SHOW ME THE MOVIES", icon = icon("film"), color = "success", style = "fill", ),
+          align = "center",
+          br(),
+          br(),
+          br(),
+          h4("OFFICIAL TITLE"),
+          textInput(inputId = "title", label = "", value = ""),
+          br(),
+          textInput(inputId = "key_words_1", label = "KEY WORDS 1", value = ""),
+          br(),
+          textInput(inputId = "key_words_2", label = "KEY WORD 2", value = ""),
+          br(),
+          textInput(inputId = "key_words_3", label = "KEY WORD 3", value = ""),
+          br(),
+          sliderInput("popularity", "MINIMUM POPULARITY", min = 0, max = 150, value = 0),
+          br(),
+          selectInput("country", "PRODUCTION COUNTRY", list(
+            "All",
+            "Africa" = c("Algeria", "Angola", "Cameroon", "Egypt", "Kenya", "Libyan Arab Jamahiriya", "Morocco", "Nigeria", "South Africa", "Tunisia"),
+            "America" = c("Argentina", "Aruba", "Bahamas", "Bolivia", "Brazil", "Canada", "Chile", "Colombia", "Dominica", "Dominican Republic", "Ecuador", "Guadaloupe", "Guyana", "Jamaica", "Mexico", "Panama", "Peru", "United States of America"),
+            "Asia-Oceania" = c("Afghanistan", "Australia", "Bhutan", "Cambodia", "China", "Fiji", "Hong Kong", "India", "Indonesia", "Iran", "Israel", "Japan", "Jordan", "Kazakhstan", "Kyrgyz Republic", "Lebanon", "Malaysia", "New Zealand", "Pakistan", "Philippines", "Singapore", "South Korea", "Taiwan", "Thailand", "Turkey", "United Arab Emirates"),
+            "Europe" = c("Austria", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Cyprus", "Czech Republic", "Denmark", "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Lithuania", "Luxembourg", "Malta", "Monaco", "Netherlands", "Norway", "Poland", "Portugal", "Romania", "Russia", "Serbia", "Serbia and Montenegro", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Ukraine", "United Kingdom")
+          ), selected = NULL, multiple = FALSE, width = NULL)
+        ),
+        mainPanel(withSpinner(gt_output("search")))
+      )
     ),
     tabPanel("PeliPopCorn", icon = icon("fire")),
     tabPanel("PeliBoring", icon = icon("info"))
@@ -133,9 +135,20 @@ ui <- fluidPage(
 
 #---------------------------------------------------------------------------------------------------------------------------------------
 
-
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+
+  # Tab 1 --> Daily 
+  output$daily <- renderTable({date <- as.POSIXct(Sys.Date(), format = "%y/%m/%d")
+  numeric_date <- as.numeric(date)
+  daily_movie <- daily_find(
+    date = numeric_date,
+    df = df
+  )
+  return(daily_movie)
+  })
+  
+  # Tab 2 --> recommend 
   
   # Use data_filter function from functions_final_project.R to filter with criteria
   data_recommended <- eventReactive(input$compute, {
@@ -150,7 +163,7 @@ server <- function(input, output, session) {
       original_language_input = input$original_language,
       spoken_languages_input = input$spoken_languages
     )
-    
+
     if (nrow(df_new_recommend) == 0) {
       shinyalert(
         title = "No movies found",
@@ -167,15 +180,15 @@ server <- function(input, output, session) {
     }
     return(df_new_recommend)
   })
-  
+
   # Prints out table (temporary)
   output$recommend <- renderTable({
     choose_movie(data_recommended())
   })
-  
-  
-  # Tab 2 --> Search
-  
+
+
+  # Tab 3 --> Search
+
   # Use data_search function from functions_final_project.R to filter search
   data_searched <- eventReactive(input$compute_2, {
     df_new_search <- data_search(
@@ -187,7 +200,7 @@ server <- function(input, output, session) {
       production_country_input = input$country,
       popularity_input = input$popularity
     )
-    
+
     if (nrow(df_new_search) == 0) {
       shinyalert(
         title = "No movies found",
@@ -199,16 +212,19 @@ server <- function(input, output, session) {
       updateTextInput(session, "key_words_3", value = "")
       updateSelectInput(session, "country", selected = "All")
       updateSliderInput(session, "popularity", value = 0)
+    } else {
+      df %>%
+        gt()
     }
     
     return(df_new_search)
   })
-  
+
   # Prints out table
   output$search <- renderTable({
     data_searched()
   })
-  
+
   # When we change tab it removes plot
   observeEvent(input$tabset, {
     # reset values of criteria and wipe table
--- functions_final_project.R
+++ functions_final_project.R
@@ -10,8 +10,9 @@ library(ggplot2)
 library(ggpubr)
 library(tibble)
 library(styler)
+library(gt)
 
-#style_dir()
+# style_dir()
 
 # We want to create a function that filters based on : 2 genres, vote_average, popularity, 2 release_date, runtime, original language, spoken languages
 
@@ -20,44 +21,44 @@ library(styler)
 
 
 data_find <- function(url) {
-  
+
   # URL in case needed for testing outside of function
   url <- "https://raw.githubusercontent.com/rengalv/Movies-Data-Analysis-Grab-a-Popcorn/master/tmdb_5000_movies.csv"
-  
+
   # Reading the csv file from the URL
   movies <- read_csv(url, col_names = TRUE, na = "NA")
-  
+
   movies <- movies[!duplicated(movies$title), ]
-  
+
   # Takes in the dataframe and column name to be transformed
   json_to_df <- function(df, column) {
     column_1 <- df[apply(df[, column], 1, nchar) > 2, ] # Checks if there is an entry
-    
+
     list_1 <- lapply(column_1[[column]], fromJSON) # Converts the JSON to a list
     values <- data.frame(unlist(lapply(list_1, function(x) paste(x$name, collapse = ",")))) # Collapsing all the values of the list as a key value pair
-    
+
     final_df <- cbind(column_1$id, column_1$title, values) # new data frame with the key and values a s columns
     names(final_df) <- c("id", "title", column)
     return(final_df)
   }
-  
+
   genres_df <- json_to_df(movies, "genres")
   keywords_df <- json_to_df(movies, "keywords")
   prod_cntry_df <- json_to_df(movies, "production_countries")
   prod_cmpny_df <- json_to_df(movies, "production_companies")
   spoken_lang_df <- json_to_df(movies, "spoken_languages")
-  
-  
+
+
   # Subset the movies dataframe by removing the JSON columns
   movies_1 <- subset(movies, select = -c(genres, keywords, production_companies, production_countries, spoken_languages))
-  
+
   # Join the columns from all the generated dataframes from previous step
   movies_df <- movies_1 %>%
     full_join(genres_df, by = c("id", "title")) %>%
     full_join(keywords_df, by = c("id", "title")) %>%
     full_join(prod_cntry_df, by = c("id", "title")) %>%
     full_join(spoken_lang_df, by = c("id", "title"))
-  
+
   return(movies_df)
 }
 
@@ -66,23 +67,23 @@ data_find <- function(url) {
 
 data_recommend <- function(df, genre_1, genre_2, vote_average_input, date_before,
                            date_after, runtime_input, original_language_input, spoken_languages_input) {
-  
+
   # Here we say if user chooses all original languages, then we will pick all the possible languages
   if (original_language_input == "All") {
     original_language_input <- ""
   }
-  
+
   # Here we say if user chooses all original languages, then we will pick all the possible languages
   if (spoken_languages_input == "All") {
     spoken_languages_input <- ""
   }
-  
+
   # Here we say if user chooses all genres, then we will pick all the possible genres
   if (genre_1 == "All" | genre_2 == "All") {
     genre_1 <- ""
     genre_2 <- ""
   }
-  
+
   movies_recommendation <- df %>%
     filter(status == "Released") %>%
     filter(grepl(pattern = genre_1, x = genres)) %>%
@@ -93,7 +94,7 @@ data_recommend <- function(df, genre_1, genre_2, vote_average_input, date_before
     filter(runtime <= runtime_input) %>%
     filter(grepl(pattern = original_language_input, x = original_language)) %>%
     filter(grepl(pattern = spoken_languages_input, x = spoken_languages))
-  
+
   return(movies_recommendation)
 }
 
@@ -113,12 +114,12 @@ choose_movie <- function(df) {
 
 
 data_search <- function(df, title_input, key_input_1, key_input_2, key_input_3, production_country_input, popularity_input) {
-  
+
   # Here we say if user chooses all genres, then we will pick all the possible genres
   if (production_country_input == "All") {
     production_country_input <- ""
   }
-  
+
   movies_search <- df %>%
     filter(grepl(pattern = title_input, x = title)) %>%
     filter(grepl(pattern = key_input_1, x = keywords)) %>%
@@ -126,37 +127,21 @@ data_search <- function(df, title_input, key_input_1, key_input_2, key_input_3, 
     filter(grepl(pattern = key_input_3, x = keywords)) %>%
     filter(popularity >= popularity_input) %>%
     filter(grepl(pattern = production_country_input, x = production_countries)) %>%
-    select(-c(budget, id, original_title, revenue, status, tagline, vote_count, keywords))
-  
-  
+    select(c(title, overview))
+
   return(movies_search)
 }
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------
 
 
-tale_maker = function(df){
-  sp500 %>%
-    filter(date >= start_date & date <= end_date) %>%
-    select(-adj_close) %>%
-    gt() %>%
-    tab_header(
-      title = "S&P 500",
-      subtitle = glue("{start_date} to {end_date}")
-    ) %>%
-    fmt_date(
-      columns = date,
-      date_style = 3
-    ) %>%
-    fmt_currency(
-      columns = c(open, high, low, close),
-      currency = "USD"
-    ) %>%
-    fmt_number(
-      columns = volume,
-      suffixing = TRUE
-    )
+daily_find = function(date, df){
+  set.seed(date)
+  random_number <- round(runif(1, min = 1, max = nrow(df)), 0)
+  random_movie <- df[random_number, ]
 }
 
+
+#------------------------------------------------------------------------------------------------------------------------------------------------
 
 
